c820305e45fbadb3c1c5cce0c56b1dff7e449377
Ryan DeVault <69012106+CtrlVaultDel@users.noreply.github.com>
2022-09-25 15:16:30 -0500

    reviewsDAO added. reviews.controller.js modified to handle updating and deleting reviews. Section for adding a review refactored to not store as many variables. index.js updated to utilize ReviewsDAO.

--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -3,24 +3,63 @@ import ReviewsDAO from "../dao/reviewsDAO.js";
 export default class ReviewsController{
     static async apiPostReview(req, res, next){
         try{
-            // Gather required information (from the request body instead of the url)
-            const movieId = req.body.movie_id;
-            const review = req.body.review;
-            const userInfo = {
-                name: req.body.name,
-                _id: req.body.user_id
-            };
-            const date = new Date();
-
-            // Wait for the response, then save it
+            // Wait for a response from the add request
             const ReviewResponse = await ReviewsDAO.addReview(
-                movieId,
-                userInfo,
-                review,
-                date
+                req.body.movie_id,
+                {
+                    name: req.body.name,
+                    _id: req.body.user_id
+                },
+                req.body.review,
+                new Date()
             );
 
             // Return the response as JSON
+            res.json({status: "success"});
+        }
+        catch(e){
+            res.status(500).json({error: e.message});
+        }
+    }
+
+    static async apiUpdateReview(req, res, next){
+        try{
+            // Wait for a response from the update request
+            const ReviewResponse = await ReviewsDAO.updateReview(
+                req.body.review_id,
+                req.body.user_id,
+                req.body.review,
+                new Date()
+            )
+
+            // Case: Error during call to ReviewsDAO.updateReview
+            let { error } = ReviewResponse;
+            if(error){
+                res.status.json({error});
+            }
+
+            // Case: Nothing was modified
+            if(ReviewResponse.modifiedCount === 0){
+                throw new Error("unable to update review. The requestor might not be the original poster.");
+            }
+
+            // Send off response as JSON
+            res.json({status: "success"})
+        }
+        catch(e){
+            res.status(500).json({error: e.message});
+        }
+    }
+
+    static async apiDeleteReview(req, res, next){
+        try{
+            // Wait for a response from the delete request
+            const ReviewResponse = await ReviewsDAO.deleteReview(
+                req.body.review_id,
+                req.body.user_id
+            )
+
+            // Send off response as JSON
             res.json({status: "success"});
         }
         catch(e){
